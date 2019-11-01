17150c2602d4278d9e0d464efdf256d46e5ce8b8
gee <special.geeee@gmail.com>
2019-11-01 16:36:35 +0100

    update vue app

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
   </div>
 </template>
 
+<style lang="css">
+@import "./assets/css/animate.min.css";
+@import "./assets/css/style.css";
+</style>
+
 <script>
 import Home from "./components/Home.vue"
 
@@ -14,14 +19,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
